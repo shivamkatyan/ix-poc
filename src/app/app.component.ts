6920c7b26e84b9dc49568877e0b73f10017a09fb
shivam <shivam.katyan@nagarro.com>
2024-02-07 13:22:07 +0530

    poc draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IxMenu } from '@siemens/ix-angular';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `
+    <ix-basic-navigation application-name="Virtual Lab" #sidebar>
+      <div class="placeholder-logo" slot="logo">
+      <ix-icon name="ai" size="32"></ix-icon>
+      </div>
+      <ix-menu #menu [enableToggleTheme]="true">
+      <ix-menu-avatar image="https://ui-avatars.com/api/?name=John+Doe">
+        <ix-menu-avatar-item label="Option 1"></ix-menu-avatar-item>
+      </ix-menu-avatar>
+        <ix-menu-item home-tab tab-icon="home"> Home </ix-menu-item>
+        <ix-menu-item tab-icon="globe"> Normal Tab </ix-menu-item>
+        <ix-menu-item tab-icon="star" disabled> Disabled Tab </ix-menu-item>
+        <ix-menu-item tab-icon="star"> With other Icon </ix-menu-item>
+        <ix-menu-item tab-icon="globe" style="display: none">
+          Should not visible
+        </ix-menu-item></ix-menu
+      >
+      <div>
+        <ng-content> </ng-content>
+      </div>
+    </ix-basic-navigation>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ix-poc';
+  @ViewChild(IxMenu) menu!: IxMenu;
+
+  ngAfterViewInit(): void {
+    this.menu.toggleMapExpand(false);
+    const el: HTMLElement | null = document.querySelector('#menu-collapse');
+
+    el?.click();
+  }
+
+  toggle() {
+    const el: HTMLElement | null = document.querySelector('#menu-collapse');
+
+    el?.click();
+    this.menu.mapExpandChange.emit(
+      new CustomEvent('', {
+        detail: false,
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IxModule } from '@siemens/ix-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IxModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
